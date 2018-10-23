ca8c5754ffec114976f2d40dd87846b8f7802bc0
suresh gandikota <suresh.gandikota@outlook.com>
2018-10-23 13:30:52 +1100

    Add project files.

---
+++ ArrayApi/Controllers/ArrayCalcController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArrayApi.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ArrayApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ArrayCalc")]
+    public class ArrayCalcController : ControllerBase
+    {
+        private readonly ILogger<ArrayCalcController> _logger;
+
+        public ArrayCalcController(ILogger<ArrayCalcController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/ArrayCalc
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new int[] { 1,2,3,4,5});
+        }
+
+        // GET: api/ArrayCalc/Reverse
+        [HttpGet("Reverse")]
+        public IActionResult Reverse([FromQuery(Name ="productIds")]int[] productIds)
+        {
+            try
+            {
+                return Ok(ArrayService<int>.Reverse(productIds));
+            }
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("DeletePart")]
+        public IActionResult DeletePart([FromQuery(Name ="position")]int position ,[FromQuery(Name = "productIds")]int[] productIds)
+        {
+            try
+            {
+                return Ok(ArrayService<int>.DeletePart(position, productIds));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+        }
+
+    }
+}
---
+++ ArrayApi/Service/ArrayService.cs
@@ -0,0 +1,59 @@
+﻿
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayApi.Service
+{
+    public static class ArrayService<T> where T : new()
+    {
+        
+
+        public static T[] Reverse(T[] inputList)
+        {
+            try
+            {
+                int size = inputList.Count<T>();
+                for (int i = 0, j = size - 1; i < j; i++, j--)
+                {
+                    var temp = inputList[i];
+                    inputList[i] = inputList[j];
+                    inputList[j] = temp;
+                }
+
+                return inputList;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Something unexpected happened.");
+            }
+            
+        }
+
+        public static T[] DeletePart(int index,T[] inputList)
+        {
+            int size = inputList.Count<T>();
+            if (index < 0 || index > size)
+            {
+                throw new IndexOutOfRangeException("Index value is out of range. please enter correct index value.");
+            }
+
+            T[] outputList = new T[size - 1];
+            int i = 0;
+            for (i = 0; i < index - 1; i++)
+            {
+                outputList[i] = inputList[i];
+            }
+
+            for (int j = index; j < size; j++)
+            {
+                outputList[i++] = inputList[j];
+            }
+
+            return outputList;
+            
+        }
+    }
+}
